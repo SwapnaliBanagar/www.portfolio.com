692946b562fbd87ce86cd6d2394107a66b257eee
DadasoBangar <dadasobanagar0906@gmail.com>
2025-02-04 16:18:35 +0530

    mobile view 320

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-resume',
   standalone: false,
-  
+
   template: `
     <div class="resume-container">
       <iframe
@@ -14,7 +14,7 @@ import { Component } from '@angular/core';
       ></iframe>
     </div>
   `,
-  styles:  [
+  styles: [
     `
       .resume-container {
         padding: 20px;
@@ -27,7 +27,7 @@ import { Component } from '@angular/core';
        @media only screen and (min-device-width: 0px) and (max-width: 320px) {
     .resume-container{
       height: 200vh;
-      width: 217%; ;
+      width:87%; ;
           }
   }
 
